2a9af03338f1366fff53a03e848e6b87c857f11f
Julian Ruano <julianrmajin@gmail.com>
2024-04-09 23:16:52 -0500

    add security with keycloak

--- api-gateway/src/main/java/com/apigateway/SecurityConfiguration.java
+++ api-gateway/src/main/java/com/apigateway/SecurityConfiguration.java
@@ -1,5 +1,5 @@
 package com.apigateway;
-/* 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,10 +8,10 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity */
+@EnableWebFluxSecurity
 public class SecurityConfiguration {
 
-    /* 
+    
     @Bean
     SecurityWebFilterChain SecurityWebFilterChain(ServerHttpSecurity httpSecurity) {
         httpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
@@ -19,6 +19,6 @@ public class SecurityConfiguration {
         httpSecurity.csrf(csrfSpec -> csrfSpec.disable());
         return httpSecurity.build();
     }
-    */
+    
     
 }
