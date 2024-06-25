e36224a31207044cb55b2bdd3de515b9cde77e97
JulianRuano <julianrmajin@gmail.com>
2024-06-25 11:37:38 -0500

    chore: Remove unused files and dependencies

--- booking/src/main/java/com/singlecode/booking/BookingApplication.java
+++ booking/src/main/java/com/singlecode/booking/BookingApplication.java
@@ -2,12 +2,8 @@ package com.singlecode.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
 public class BookingApplication {
 
 	public static void main(String[] args) {
--- booking/src/main/java/com/singlecode/booking/controller/BookingController.java
+++ booking/src/main/java/com/singlecode/booking/controller/BookingController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.singlecode.booking.client.StockClient;
 import com.singlecode.booking.dto.OrderDTO;
 import com.singlecode.booking.entity.Order;
 import com.singlecode.booking.repository.IOrderRepository;
@@ -20,32 +17,15 @@ public class BookingController {
     @Autowired
     private IOrderRepository orderRepository;
 
-    @Autowired
-    private StockClient stockClient;
-
     @PostMapping("/order")
-    @HystrixCommand(fallbackMethod = "fallbackToStockService")
     public String saveOrder(@RequestBody OrderDTO orderDTO) {
 
-        boolean inStock = orderDTO.getOrderItems().stream()
-                .allMatch(orderItem -> stockClient.strockAvailability(orderItem.getCode()));
+        Order order = new Order();
+        order.setOrderNo(UUID.randomUUID().toString());
+        order.setOrderItems(orderDTO.getOrderItems());
+        orderRepository.save(order);
 
-        if(inStock) {
-            Order order = new Order();
-
-            order.setOrderNo(UUID.randomUUID().toString());
-            order.setOrderItems(orderDTO.getOrderItems());
-
-            orderRepository.save(order);
-
-            return "Order Saved";
-        }
-
-        return "Order Cannot be Saved";
+        return "Order saved successfully";
     }
 
-    @SuppressWarnings("unused")
-    private String fallbackToStockService() {
-        return "Something went wrong, please try after some time";
-    }
 }
