e082f0fde34819ca449e6433e4d0d7e0b6cb6c08
JulianRuano <julianrmajin@gmail.com>
2024-06-25 12:20:45 -0500

    chore: Update StockController with GET, PUT, and POST methods for stock availability and updates

--- stock/src/main/java/com/stock/controller/StockController.java
+++ stock/src/main/java/com/stock/controller/StockController.java
@@ -3,12 +3,18 @@ package com.stock.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stock.entity.StockEntity;
 import com.stock.repository.StockRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 @RequestMapping("/stock")
@@ -17,13 +23,38 @@ public class StockController {
     @Autowired
     private StockRepository stockRepository;
 
-    @RequestMapping("/{code}")
-    public boolean strockAvailability(@PathVariable String code) {
+    @GetMapping("/{code}")
+    public ResponseEntity<StockEntity> strockAvailability(@PathVariable String code) {
         Optional<StockEntity> stock = stockRepository.findByCode(code);
 
         stock.orElseThrow(() -> new RuntimeException("Stock not found"+ code));
 
-        return stock.get().getQuantity() > 0;
+        return ResponseEntity.ok(stock.get());
     }
+
+    @PutMapping("/{code}/{quantity}")
+    public ResponseEntity<StockEntity> updateStock(@RequestParam String code, @RequestParam Integer quantity) {
+        Optional<StockEntity> stock = stockRepository.findByCode(code);
+
+        stock.orElseThrow(() -> new RuntimeException("Stock not found"+ code));
+
+        stock.get().setQuantity(stock.get().getQuantity() - quantity);
+
+        stockRepository.save(stock.get());
+
+        return ResponseEntity.ok(stock.get());
+    }
+
+    @PostMapping("/{code}/{quantity}")
+    public ResponseEntity<StockEntity> createStock(@RequestParam String code, @RequestParam Integer quantity) {
+        StockEntity stock = new StockEntity();
+        stock.setCode(code);
+        stock.setQuantity(quantity);
+
+        stockRepository.save(stock);
+
+        return ResponseEntity.ok(stock);
+    }
+
     
 }
